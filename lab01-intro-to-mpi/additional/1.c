e08be9bb95e2b10eb04353a9ede3d676197f74c2
Aditya Mathpal <adimathpal@gmail.com>
2026-01-09 21:02:24 +0530

    lab 1 complete

--- lab01-intro-to-mpi/additional/1.c
+++ lab01-intro-to-mpi/additional/1.c
@@ -1,3 +1,7 @@
+/*
+reverse digits of numbers in array
+*/
+
 #include"mpi.h"
 #include<stdio.h>
 
@@ -17,8 +21,21 @@ int main(int argc, char *argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     int arr[] = {18, 523, 301, 1234, 2, 14, 108, 150, 1928};
     arr[rank%9] = reverse(arr[rank%9]);
-    for(int i = 0; i < 9; i++) printf("%d  ", arr[i]);
+    printf("%d reversed: %d  ", arr[rank], reverse(arr[rank]));
     printf("\n");
     MPI_Finalize();
     return 0;
 }
+
+/*
+output:
+325 reversed: 523  
+4321 reversed: 1234  
+2 reversed: 2  
+801 reversed: 108  
+51 reversed: 15  
+81 reversed: 18  
+103 reversed: 301  
+41 reversed: 14  
+8291 reversed: 1928
+*/
